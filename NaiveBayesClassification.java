19efa778a2c6f0c44a32c3a04bedfba27c308b5d
jayem1225 <jacobmartinez18@gmail.com>
2018-12-04 22:22:38 -0500

    Pushing update for classifier. Currently only correct 1/3 the time...seems to be randomly correct.

--- NaiveBayesClassification.java
+++ NaiveBayesClassification.java
@@ -34,9 +34,11 @@ public class NaiveBayesClassification {
 	// Cols = constants
 	private static double[][] meanArr;
 	private static double[][] varianceArr;
+	private static double[] classArr; // Stores class representation as decimal
 	
 	private static Object[][] rawData;
 	
+	// Adds classifier to classificationArr if one does not already exist
 	private static int addClassifier(String classifier) {
 		if (getClassifierIndex(classifier) < 0) {
 			String[] tempArr = new String[classificationArr.length+1];
@@ -54,21 +56,13 @@ public class NaiveBayesClassification {
 		for (int i=0; i<classificationArr.length; ++i)
 			if (classificationArr[i].equals(classifier))
 				return i;
+		
 		return -1;
 	}
 	
-	private static int getClassificationCount(int classIndex) {
-		int count = 0;
-		
-		for (int i=0; i<rawData.length; ++i)
-			if ((int)rawData[i][CLASSIFICATION] == classIndex)
-				++count;
-		
-		return count;
-	}
-	
+	// Returns array of feature values for the given class
 	private static double[] getFeatureValsByClass(int classIndex, int feature) {
-		double[] values = new double[getClassificationCount(classIndex)];
+		double[] values = new double[rawData.length];
 		
 		int index = 0;
 		for (int i=0; i<rawData.length; ++i)
@@ -77,9 +71,14 @@ public class NaiveBayesClassification {
 				++index;
 			}
 		
-		return values;
+		double[] valuesTrimmed = new double[index];
+		for (int i=0; i<valuesTrimmed.length; ++i)
+			valuesTrimmed[i] = values[i];
+		
+		return valuesTrimmed;
 	}
 	
+	// Loads data from CSV into a 2 dimensional array of Objects
 	private static Object[][] loadDataFromFile(String fileName) {
 		Object[][] dataArr = null;
 
@@ -119,6 +118,7 @@ public class NaiveBayesClassification {
 		return dataArr;
 	}
 	
+	// Shuffles list of data
 	private static Object[][] shuffleData(Object[][] rawData) {
 		Random rnd = ThreadLocalRandom.current();
 		Object[][] shuffledData = rawData;
@@ -135,32 +135,95 @@ public class NaiveBayesClassification {
 		return shuffledData;
 	}
 	
+	// Trains data set by taking in a set of data and filling trained data arrays
+	// for later use in probability calculation
 	private static void trainNaiveBayes(Object[][] trainingData) {
+		classArr = new double[classificationArr.length];
 		meanArr = new double[classificationArr.length][4];
 		varianceArr = new double[classificationArr.length][4];
+		double sum = 0.0;
+		double [] classFeatureArr;
+		
+		// Fill class Array
+		for (int clsfctn = 0; clsfctn<classificationArr.length; ++clsfctn) {
+			int count = 0;
+			for (int i=0; i<trainingData.length; ++i)
+				if (trainingData[i][CLASSIFICATION].equals(classificationArr[clsfctn]))
+					++count;
+			classArr[clsfctn] = (double)count/trainingData.length;
+		}
+				
 		
 		// Fill Average Array
 		for (int clsfctn = 0; clsfctn<classificationArr.length; ++clsfctn)
-			for (int feature = 0; feature<4; ++feature)
-				meanArr[clsfctn][feature] = Arrays.stream(getFeatureValsByClass(clsfctn, feature)).average().getAsDouble();
+			for (int feature = 0; feature<4; ++feature) {
+				classFeatureArr = getFeatureValsByClass(clsfctn, feature);
+				for (int i = 0; i < classFeatureArr.length; ++i)
+					sum += classFeatureArr[i];
+				meanArr[clsfctn][feature] = sum/((double)classFeatureArr.length);
+			}
+		
+		sum = 0.0;
 		
 		// Fill Variance Array
+		for (int clsfctn = 0; clsfctn<classificationArr.length; ++clsfctn)
+			for (int feature = 0; feature<4; ++feature) {
+				classFeatureArr = getFeatureValsByClass(clsfctn, feature);
+				for (int i = 0; i < classFeatureArr.length; ++i)
+					sum += (classFeatureArr[i] - meanArr[clsfctn][feature]) * (classFeatureArr[i] - meanArr[clsfctn][feature]);
+				varianceArr[clsfctn][feature] = Math.sqrt(sum/((double)classFeatureArr.length));
+			}
 	}
 	
-	private static void testNaiveBayes(Object[][] testData) {
-		if (!trained)
-			return;
+	// Takes in a set of test values and returns the percentage of values guessed correctly
+	private static double testNaiveBayes(Object[][] testData) {
+		int countCorrect = 0;
+		
+		for (int i=0; i<testData.length; ++i) {
+			double[] testFeature = new double[testData[i].length-1];
+			for (int feature = 0; feature<testFeature.length; ++feature)
+				testFeature[feature] = (double)testData[i][feature];
+			if (predictClassifier(testFeature) == (int)testData[i][CLASSIFICATION])
+				++countCorrect;
+		}
+
+		return (double)countCorrect/testData.length*100;
 	}
 	
-	private static int prediction() {
-		if (!trained)
-			return -1;
+	// Takes in feature array and returns best classification guess as an index value
+	private static int predictClassifier(double[] featureArr) {
+		double[] estimates = new double[classificationArr.length];
+		
+		// Test all possible classifications and store probabilities
+		for (int i=0; i<classificationArr.length; ++i)
+			estimates[i] = calculateProbability(i, featureArr);
+		
+		// Find and return best guess
+		int bestGuess = 0;
+		for (int i=1; i<estimates.length; ++i)
+			if (estimates[i] > estimates[bestGuess])
+				bestGuess = i;
+		
+		return bestGuess;
+	}
+	
+	private static double calculateProbability(int classifier, double[] values) {
+		double probability = classArr[classifier];
+		for (int feature = 0; feature<4; ++feature)
+			probability *= probabilityDensityFunction(feature, classifier, values[feature]);
+		
+		return probability;
+	}
+	
+	private static double probabilityDensityFunction(int feature, int classifier, double x) {
+		double stdDevSqrd = varianceArr[classifier][feature] * varianceArr[classifier][feature];
+		double xMeanDiffSqrd = (x - meanArr[classifier][feature]) * (x - meanArr[classifier][feature]);
+		return ( 1/Math.sqrt(2 * Math.PI * stdDevSqrd) ) * ( Math.exp(-xMeanDiffSqrd / (2 * stdDevSqrd) ) );
 	}
 	
 	public static void main(String[] args) {
 		Object[][] shuffledData, testSet, trainingSet;
 		classificationArr = new String[0];
-		boolean trained = false;
 		
 		rawData = loadDataFromFile("iris.data");
 		shuffledData = shuffleData(rawData);
@@ -168,13 +231,14 @@ public class NaiveBayesClassification {
 		trainingSet = Arrays.copyOfRange(shuffledData, shuffledData.length/2, shuffledData.length); 
 		
 		trainNaiveBayes(trainingSet);
-		testNaiveBayes(testSet);
-	
+
 		System.out.println("Training success!\nClassifications loaded: ");
 		for (int i=0; i<classificationArr.length; ++i)
 			System.out.println("\t" + classificationArr[i]);
 		
-		}
+		double testResults = testNaiveBayes(testSet);
+		System.out.println("Test complete. ");
+		System.out.println("Percentage properly categorized: " + testResults);
 	}
 
 }
